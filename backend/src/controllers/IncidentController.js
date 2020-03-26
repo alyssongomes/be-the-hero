6f13f2718d7058b777b286ce8ca50c36ecd4073d
Alysson Gomes <alyssonasn02@gmail.com>
2020-03-26 00:33:23 -0300

    frontend da aplicação

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -25,7 +25,7 @@ module.exports = {
     async create(request, response){
         const { title, description,  value } = request.body;
         const ong_id = request.headers.authorization;
-
+        console.log(request.headers);
         const [ id ] = await connection("incidents").insert({
             title,
             description,
